3d43487c2db34c0a916f72a1497289c264c66af8
Raz1990 <raz2712@gmail.com>
2018-10-02 20:07:31 +0300

    added serverAPI for AJAX calls

---
+++ src/ServerAPI.ts
@@ -0,0 +1,9 @@
+export default class ServerAPI {
+  private apiKey = '&apikey=b6463727';
+  private baseUrl = 'http://www.omdbapi.com/?t=';
+  
+  public getMovie(titleName: string) {
+    return fetch(this.baseUrl + titleName + this.apiKey)
+          .then(res => res.json());
+  }
+}
