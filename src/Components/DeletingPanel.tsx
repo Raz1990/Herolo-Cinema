4144f21ab21f0aa2eccf92aea95fa1e541caf6d8
Raz1990 <raz2712@gmail.com>
2018-10-04 12:30:04 +0300

    added DeletingPanel componenet

---
+++ src/Components/DeletingPanel.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import '../CSS/cssButton.css';
+import '../CSS/cssModal.css';
+import Modal from './Modal';
+
+interface IDeleteProps {
+  cancelCallback: () => void,
+  submitCallback: () => void,
+}
+
+class DeletingPanel extends React.Component<IDeleteProps,any> {
+  constructor(props: IDeleteProps){
+    super(props);
+  }
+
+  public render(){
+    return (  
+      <Modal>
+        <section className='header'>
+          <span>
+            <label>Are you sure you want to delete this movie?</label>
+          </span>
+        </section>
+        <section className='buttonsWrapper'>
+          <button className='btn accept' onClick={this.delete}>Delete</button>
+          <button className='btn cancel' onClick={this.props.cancelCallback}>Cancel</button>
+        </section>
+    </Modal>
+    )
+  }
+
+  private delete = () => {
+    this.props.submitCallback();
+  }
+}
+
+export default DeletingPanel;
