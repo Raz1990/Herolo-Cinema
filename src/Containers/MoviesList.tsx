6c13978ee37e411670ae1eea80e1e5529e7a38df
Raz1990 <raz2712@gmail.com>
2018-10-04 12:27:40 +0300

    updated MoviesList and added css

--- src/Containers/MoviesList.tsx
+++ src/Containers/MoviesList.tsx
@@ -1,18 +1,33 @@
 import * as React from 'react';
+import EmptyItem from '../Components/EmptyItem';
+import MovieItem from '../Components/MovieItem';
+import '../CSS/cssMovieList.css';
+import Helpers from '../helpers';
 
-class MoviesList extends React.Component<{},any> {
-  constructor(props: {}){
-    super(props);  
+interface IMovieListProps {
+  moviesJSONList: []
+}
 
-    this.state = {
-      moviesList: []
-    };
+class MoviesList extends React.Component<IMovieListProps,any> {
+  constructor(props: IMovieListProps){
+    super(props);
   }
 
-  public render() {
+  public formatList(){
+    let idCount = 1;
+    const formatedList = this.props.moviesJSONList.map((movieJson: any, idx: any) => {
+      const movie = Helpers.moviefy(movieJson, idCount++);
+      return <MovieItem key={idx} movie={movie} />;
+    });
+
+    return formatedList;
+  }
+  
+    public render() {
     return (
       <section className="movie-list">
-        <h1> placeholder </h1>
+        {this.formatList()}
+        <EmptyItem/>
       </section>
     );
   }
