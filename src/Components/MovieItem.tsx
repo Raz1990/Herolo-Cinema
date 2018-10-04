72b16789c071411f4c1eca570cdf048a2c269ada
Raz1990 <raz2712@gmail.com>
2018-10-04 12:25:35 +0300

    updated MovieItem and added css

--- src/Components/MovieItem.tsx
+++ src/Components/MovieItem.tsx
@@ -1,19 +1,94 @@
 import * as React from 'react';
+import '../CSS/cssButton.css';
+import '../CSS/cssMovieItem.css';
 import IMovie from '../Interfaces/IMovie';
+import Button from './Button';
+import DeletingPanel from './DeletingPanel';
+import MovieDetailes from './MovieDetailes';
 
 interface IMovieItemProps {
-    movie: IMovie
+  movie: IMovie
 }
 
-class MovieItem extends React.Component<IMovieItemProps,{}> {
+interface IMovieItemState {
+  panelToShow: string
+}
+
+class MovieItem extends React.Component<IMovieItemProps,IMovieItemState> {
   constructor(props: IMovieItemProps) {
-      super(props);
+    super(props);
+
+    this.state = {
+      panelToShow: '',
+    };
   }
 
   public render() {
+    let modal: any;
+    
+    switch (this.state.panelToShow) {
+      case 'edit':
+        modal = <MovieDetailes  action='edit'
+                                cancelCallback={this.cancelModal} 
+                                submitCallback={this.editMovie} 
+                                movieItem={this.props.movie}/>;
+        break;
+      case 'delete':
+        modal = <DeletingPanel  cancelCallback={this.cancelModal} 
+                                submitCallback={this.deleteMovie} />;
+        break;
+      default:
+        modal = null;
+    }
+    
     return (
-        <section className='movie-item'> Title: {this.props.movie.getTitle()} </section>
+      <section className='movie-item'>
+        <ul>
+          <li>Id: {this.props.movie.getId()}</li>
+          <li>Title: {this.props.movie.getTitle()}</li>
+          <li>Year: {this.props.movie.getYear()}</li>
+          <li>Runtime: {this.props.movie.getRunTime()}</li>
+          <li>Genre: {this.props.movie.getGenre()}</li>
+          <li>Director: {this.props.movie.getDirector()}</li>
+        </ul>
+        <section className='buttons'>
+          <Button contentSTR='Edit' className={'btn edit'} callbackFunc={this.showEditModal}/>
+          <Button contentSTR='Delete' className={'btn delete'} callbackFunc={this.showDeleteModal}/>
+        </section>
+        {modal ? modal : <div/>}
+      </section>
     );
+  }
+
+  private toggleModal = (panel:string) => {
+    this.setState({
+      panelToShow: panel
+    });
+  }
+
+  private cancelModal = () => {
+    this.setState({
+      panelToShow: ''
+    });
+  };
+
+  private showEditModal = () => {
+    this.toggleModal('edit');
+  }
+
+  private showDeleteModal = () => {
+    this.toggleModal('delete');
+  }
+
+  private editMovie = (editedMovie: IMovie) => {
+    console.log('editing!');
+    console.log(editedMovie);
+    this.cancelModal();
+  }
+
+  private deleteMovie = () => {
+    alert(this.props.movie.getId() + ' to be deleted');
+    this.cancelModal();
   }
 }
 
