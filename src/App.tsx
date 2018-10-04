5fe33272abfcbbc06c0b421f29097936dbb1e8ed
Raz1990 <raz2712@gmail.com>
2018-10-04 12:18:42 +0300

    updated App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,76 @@
 import * as React from 'react';
 import './App.css';
-
+import {Loader} from './Components/Loader';
 import MoviesList from './Containers/MoviesList';
+import ServerAPI from './ServerAPI';
 
-class App extends React.Component {
+interface IAppState {
+  isLoading: number,
+  moviesList: []
+}
+
+class App extends React.Component<{},IAppState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      isLoading: 0,
+      moviesList: []
+    }
+  }
+
   public render() {
+    const list = (
+      <MoviesList moviesJSONList={this.state.moviesList}/>
+    );
+
     return (
       <section id='Main'>
-        <MoviesList/>
+        <h1>Welcome to Herolo Cinema City!</h1>
+        {this.state.isLoading ? (<Loader/>) : list}
       </section>
     );
+  }
+
+  public componentDidMount() {
+    this.startLoading();
+
+    const serverApi = new ServerAPI();
+    const initialMoviesNames = ['guardians of the galaxy', 
+                                'guardians of the galaxy vol 2', 
+                                'Avengers', 
+                                'Avengers age of ultron', 
+                                'Avengers infinity',
+                                'Captain America the first avenger', 
+                                'Captain America the Winter Soldier', 
+                                'Captain America Civil War', 
+                                'Thor ragnarok'];
+    
+    for (const movieName of initialMoviesNames) {
+      serverApi.getMovie(movieName)
+      .then((movieJson) => {
+        this.setState((prevState: any)=>{
+          return {moviesList: prevState.moviesList.concat(movieJson)};
+        });
+        if (this.state.moviesList.length === initialMoviesNames.length) {
+          this.stopLoading();
+        }
+      });
+    }
+  }
+
+  private startLoading() {
+    this.setState((prevState: IAppState) => ({
+      ...prevState,
+      isLoading: ++prevState.isLoading
+     }));
+    }
+
+  private stopLoading() {
+  this.setState((prevState: IAppState) => ({
+      ...prevState,
+      isLoading: --prevState.isLoading
+    }));
   }
 }
 
