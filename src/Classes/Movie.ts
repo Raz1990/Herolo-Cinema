9bdc28028ad4fab7a7858dbc5a772ec76d0e65ce
Raz1990 <raz2712@gmail.com>
2018-10-04 12:19:43 +0300

    updated Movie class

--- src/Classes/Movie.ts
+++ src/Classes/Movie.ts
@@ -1,7 +1,7 @@
 import IMovie from '../Interfaces/IMovie';
 
 class Movie implements IMovie {
-  constructor(private id: number, private title:string, private year: string, private runTime:number, private genre:string, private director: string ) {}
+  constructor(private id: number, private title:string, private year: string, private runTime:string, private genre:string, private director: string ) {}
 
   public getId() {
     return this.id;
@@ -28,7 +28,7 @@ class Movie implements IMovie {
   public setYear(year: string) {
     this.year = year;
   }
-  public setRunTime(runTime: number) {
+  public setRunTime(runTime: string) {
     this.runTime = runTime;
   }
   public setGenre(genre: string) {
